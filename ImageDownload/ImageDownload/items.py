3e2a74cc3dfc0dd8374fcd366be379b6c32d021a
Sasha <evlentev_a@ukr.net>
2022-08-16 14:16:13 +0300

    Init

---
+++ ImageDownload/ImageDownload/items.py
@@ -0,0 +1,13 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ImageItem(Item):
+    # image_urls = Field()
+    # images = Field()
+    pass
+
